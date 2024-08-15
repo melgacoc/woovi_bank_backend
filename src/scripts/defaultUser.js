ae3c1f91f33f094aa35712ea883f2d493edf4931
Cláudio Melgaço <claudio19a@hotmail.com>
2024-08-15 04:28:17 -0300

    Ajustando improtação da usersModel

--- src/scripts/defaultUser.js
+++ src/scripts/defaultUser.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const User = require('../models/userModel'); 
+const User = require('../models/usersModel'); 
 
 const createDefaultUser = async () => {
   try {
